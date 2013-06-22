964b9cfd76a5f953f4b475d2523b70e23453ca52
Philip Benton <philip.benton@itsgoodpractice.com>
2013-06-22 19:38:18 +0100

    Added main framework files
    Mainly SCSS files.

--- README.md
+++ README.md
@@ -2,3 +2,11 @@ Mixture Prototyping Boilerplate
 ===============================
 
 Get up and running quickly with this prototyping boilerplate constructed around Mixture.io
+
+## Browser support
+
+* Google Chrome
+* Mozilla Firefox 4+
+* Apple Safari 5+
+* Opera 12+
+* Internet Explorer 8+
---
+++ compass/config.rb
@@ -0,0 +1,17 @@
+# Compass configuration file.
+
+# We also support plugins and frameworks, please read the docs http://docs.mixture.io/preprocessors#compass
+
+project_path    = "/Users/philipbenton/Dropbox/Projects/Mixture Prototyping Boilerplate/"
+
+# Important! change the paths below to match your project setup. Set to the root of your project when deployed.
+css_dir         = "public/stylesheets"
+sass_dir        = "public/scss"
+images_dir      = "public/images"
+javascripts_dir = "public/javascripts"
+fonts_dir       = "public/fonts"
+svg_dir         = "public/svg"
+
+line_comments   = false # if debugging (or using Mixture chrome extension - set this to true)
+cache           = true
+color_output    = false # required for Mixture
